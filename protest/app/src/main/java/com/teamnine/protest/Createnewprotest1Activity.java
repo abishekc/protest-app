6ae967b6f0fe719b4216cff42fde162f239e3eb8
Forrest Kim <forrestkim00@berkeley.edu>
2020-11-20 18:02:55 -0800

    [Front end] Login + Create new Protest First Push
    - Have to import logo + fonts + tweak sizes a little

---
+++ protest/app/src/main/java/com/teamnine/protest/Createnewprotest1Activity.java
@@ -0,0 +1,14 @@
+package com.teamnine.protest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Createnewprotest1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createnewprotest1);
+    }
+}
