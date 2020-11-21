ef6e5af85208ec850f127c7de6eb9cd8dc8aaea8
Areeb Khalfay <arkhalfay@yahoo.com>
2020-11-20 20:03:26 -0800

    Front End - Create New Protest 2 and 3 done

---
+++ protest/app/src/main/java/com/teamnine/protest/CreateNewProtest3Activity.java
@@ -0,0 +1,14 @@
+package com.teamnine.protest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateNewProtest3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_new_protest3);
+    }
+}
