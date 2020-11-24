0afbde9db7df03ffa40e1f08df77de88e0e0be9d
Forrest Kim <forrestkim00@berkeley.edu>
2020-11-23 19:14:25 -0800

    [Front End] Recycler View for Events

---
+++ protest/app/src/main/java/com/teamnine/protest/MyprotestsActivity.java
@@ -0,0 +1,14 @@
+package com.teamnine.protest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyprotestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myprotests);
+    }
+}
