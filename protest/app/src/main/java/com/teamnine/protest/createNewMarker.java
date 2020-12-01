403bd40f63a61d893464d84e8c76b5565ce36882
Forrest Kim <forrestkim00@berkeley.edu>
2020-11-30 21:00:50 -0800

    [FRONT END] Create New Marker Page 🧦

---
+++ protest/app/src/main/java/com/teamnine/protest/createNewMarker.java
@@ -0,0 +1,14 @@
+package com.teamnine.protest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createNewMarker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_marker);
+    }
+}
