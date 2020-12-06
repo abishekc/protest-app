62efb093b40f30424eede65f2e78289d0fe7ff70
Abishek Chozhan <abishekchozhan@berkeley.edu>
2020-12-05 22:25:01 -0800

    finished latest update and adding update abilities

--- protest/app/src/main/java/com/teamnine/protest/EventItem.java
+++ protest/app/src/main/java/com/teamnine/protest/EventItem.java
@@ -10,10 +10,16 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class EventItem extends AppCompatActivity {
 
@@ -35,6 +41,7 @@ public class EventItem extends AppCompatActivity {
         TextView eventDescription = findViewById(R.id.description_input);
         TextView sentimentScore = findViewById(R.id.sentiment);
         TextView editTitle = findViewById(R.id.edit_title);
+        final TextView updateText = findViewById(R.id.update_text);
 
         ImageButton editButton = (ImageButton) findViewById(R.id.edit_button);
 
@@ -44,6 +51,32 @@ public class EventItem extends AppCompatActivity {
         eventEndDate.setText(passedEvent.getEndDate());
         eventDescription.setText(passedEvent.getDescription());
         sentimentScore.setText(String.valueOf(passedEvent.getSentiment()));
+
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("events").child(passedEvent.getId()).child("latest_update").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.getValue() != null) {
+                    mDatabase.child("updates").child(snapshot.getValue().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            ProtestFeed latestUpdate = snapshot.getValue(ProtestFeed.class);
+                            updateText.setText(latestUpdate.getDescription());
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         Button feed_button = findViewById(R.id.feed_button);
         feed_button.setOnClickListener(new View.OnClickListener() {
